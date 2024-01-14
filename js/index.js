45188ea6bc0ed3f4a3f4d08e85b1f5a589a96689
Aliaksei Karzhou <alexius90e@gmail.com>
2024-01-14 19:37:39 +0300

    feat: added menus

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const mainPageSwiper = new Swiper(".main-page .swiper", {
   allowTouchMove: false,
-  effect: 'fade',
+  effect: "fade",
   fadeEffect: {
-    crossFade: true
+    crossFade: true,
   },
 });
 
@@ -14,4 +14,36 @@ navMenuItems.forEach((navMenuItem, index) => {
     event.currentTarget.classList.add("active");
     mainPageSwiper.slideTo(index);
   });
+});
+
+const sidebarMenus = document.querySelectorAll(".sidebar__menu");
+
+sidebarMenus.forEach((sidebarMenu) => {
+  sidebarMenu.addEventListener("click", (event) => {
+    const isToggler = event.target.classList.contains("sidebar__menu-list-item-toggler");
+    if (isToggler) {
+      event.target.parentElement.classList.toggle("active");
+    }
+  });
+});
+
+const headerCatalogBurger = document.querySelector(".header__catalog-burger");
+const footerCatalogBurger = document.querySelector(".footer__buttons .button_burger");
+
+const sidebarMenu = document.querySelector(".sidebar__menu");
+const headerNav = document.querySelector(".header__nav");
+
+
+headerCatalogBurger.addEventListener("click", () => {
+  sidebarMenu.classList.toggle("active");
+  headerCatalogBurger.classList.toggle("active");
+  document.body.classList.toggle("active");
+  headerNav.classList.remove("active");
+});
+
+footerCatalogBurger.addEventListener("click", () => {
+  headerNav.classList.toggle("active");
+  footerCatalogBurger.classList.toggle("active");
+  document.body.classList.toggle("active");
+  sidebarMenu.classList.remove("active");
 });
